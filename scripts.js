600db59e2acc8eff8eec6fb1b94486dfbc7ff1b3
Abrar74774 <abrarshahriarhossain@gmail.com>
2019-11-06 22:36:49 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,86 @@
+
+
+//SNIPPET
+
+// Cache selectors
+var lastId,
+ topMenu = $("#mainNav"),
+ topMenuHeight = topMenu.outerHeight()+1,
+ // All list items
+ menuItems = topMenu.find("a"),
+ // Anchors corresponding to menu items
+ scrollItems = menuItems.map(function(){
+   var item = $($(this).attr("href"));
+    if (item.length) { return item; }
+ });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 850);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});
+
+
+//SNIPPET END
+
+$(function() {
+  //caches a jQuery object containing the header element
+  var header = $("nav");
+  var wall = document.getElementById("home");
+  console.log(header);
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 50) {
+          header.addClass("is-sticky");
+          wall.style.top = "120px";
+          
+          
+      } else {
+          header.removeClass("is-sticky");
+          wall.style.top = "0px";
+      }
+  });
+});
+
+
+/*Testimonial*/
+$(document).ready(function(){
+  $("#testimonial-slider").owlCarousel({
+      items:1,
+      itemsDesktop:[1000,1],
+      itemsDesktopSmall:[979,1],
+      itemsTablet:[769,1],
+      pagination:true,
+      transitionStyle:"goDown",
+      autoplay:true
+  });
+});
