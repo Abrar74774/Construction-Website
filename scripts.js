94354cc64941c9e328693393bcb4a605ac3d36ba
Abrar74774 <abrarshahriarhossain@gmail.com>
2020-06-08 16:53:07 +0300

    added sidebar

--- scripts.js
+++ scripts.js
@@ -1,86 +1,68 @@
+var navbar = document.getElementById('navbar');
+var home = document.getElementById('home');
+var about = document.getElementById('about');
+var services = document.getElementById('services');
+var projects = document.getElementById('projects');
+var testim = document.getElementById('testimonial');
+var contact = document.getElementById('contact');
+let sidebar = document.getElementById("sidebar");
+let menu = document.querySelector(".menu");
+let overlay = document.getElementById("moberlay");
 
-
-//SNIPPET
-
-// Cache selectors
-var lastId,
- topMenu = $("#mainNav"),
- topMenuHeight = topMenu.outerHeight()+1,
- // All list items
- menuItems = topMenu.find("a"),
- // Anchors corresponding to menu items
- scrollItems = menuItems.map(function(){
-   var item = $($(this).attr("href"));
-    if (item.length) { return item; }
- });
-
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-menuItems.click(function(e){
-  var href = $(this).attr("href"),
-      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-  $('html, body').stop().animate({ 
-      scrollTop: offsetTop
-  }, 850);
-  e.preventDefault();
+menu.addEventListener("click", function () {
+  overlay.style.opacity = "0.5";
+  overlay.style.zIndex = "999999"
+  sidebar.style.left = "0px";
 });
 
-// Bind to scroll
-$(window).scroll(function(){
-   // Get container scroll position
-   var fromTop = $(this).scrollTop()+topMenuHeight;
-   
-   // Get id of current scroll item
-   var cur = scrollItems.map(function(){
-     if ($(this).offset().top < fromTop)
-       return this;
-   });
-   // Get the id of the current element
-   cur = cur[cur.length-1];
-   var id = cur && cur.length ? cur[0].id : "";
-   
-   if (lastId !== id) {
-       lastId = id;
-       // Set/remove active class
-       menuItems
-         .parent().removeClass("active")
-         .end().filter("[href=#"+id+"]").parent().addClass("active");
-   }                   
-});
+overlay.addEventListener("click", function () {
+  setTimeout('overlay.style.zIndex = "-1";', 400)
+  overlay.style.opacity = "0";
+  sidebar.style.left = "-70%";
+})
 
+window.addEventListener('scroll', function () {
+  if (window.innerWidth > 845) {
+    if (window.scrollY >= 34) {
+      navbar.style.position = 'fixed'
+      navbar.style.top = 0;
+      home.style.marginTop = '120px'
+    } else {
+      navbar.style.position = 'relative';
+      navbar.style.top = 'auto'
+      home.style.marginTop = '0'
+    }
+    console.log(window.scrollY)
+  }
+})
+//==================== INTERSECTION OBSERVER ===========
+var options = {
+  threshold: '-33% 0%'
+}
 
-//SNIPPET END
+var observer = new IntersectionObserver(function (entries) {
+  console.log(entries);
+  if (entries.filter(entry => entry.isIntersecting)) {  //heeeerrrrreee
+  }
+}, options)
 
-$(function() {
-  //caches a jQuery object containing the header element
-  var header = $("nav");
-  var wall = document.getElementById("home");
-  console.log(header);
-  $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
+observer.observe(home);
+observer.observe(about);
+observer.observe(services);
+observer.observe(testim);
+observer.observe(contact);
 
-      if (scroll >= 50) {
-          header.addClass("is-sticky");
-          wall.style.top = "120px";
-          
-          
-      } else {
-          header.removeClass("is-sticky");
-          wall.style.top = "0px";
-      }
-  });
-});
 
 
 /*Testimonial*/
-$(document).ready(function(){
+$(document).ready(function () {
   $("#testimonial-slider").owlCarousel({
-      items:1,
-      itemsDesktop:[1000,1],
-      itemsDesktopSmall:[979,1],
-      itemsTablet:[769,1],
-      pagination:true,
-      transitionStyle:"goDown",
-      autoplay:true
+    items: 1,
+    itemsDesktop: [1000, 1],
+    itemsDesktopSmall: [979, 1],
+    itemsTablet: [769, 1],
+    pagination: true,
+    transitionStyle: "goDown",
+    autoplay: true
   });
 });
